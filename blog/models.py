154bb9146a5a95a2701c4c0d9e96e33137ba8293
revemar <revemargrace@gmail.com>
2024-03-09 14:45:09 +0800

    Modified templates to display posts from database.

--- blog/models.py
+++ blog/models.py
@@ -7,8 +7,9 @@ class Post(models.Model):
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
-def publish(self):
+    
+    def publish(self):
         self.published_date = timezone.now()
         self.save()
-def __str__(self):
+    def __str__(self):
         return self.title
